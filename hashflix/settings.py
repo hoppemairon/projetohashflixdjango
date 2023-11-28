9bb2ef2e3ea8c6c87abf8febece485723d8f47c5
Mairon Hoppe <hoppe.mairon@gmail.com>
2023-11-27 22:08:42 -0300

    Alteração do DATABASE_URL

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -87,9 +87,8 @@ DATABASES = {
     }
 }
 import dj_database_url
-import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "postgresql://postgres:6fF6d512d3GCGF4--*-ebc1*C62CB5fG@viaduct.proxy.rlwy.net:51770/railway"
 if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
